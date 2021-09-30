7d880bc121147686544765a7af62e107927c4e2e
Zach Hill <zachhill7714@gmail.com>
2021-09-30 19:43:51 -0400

    t

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 var game = {
-    buns = 0.0,
-    multiplier = 1.0,
+    buns: 0.0,
+    multiplier: 1.0,
     buildings = {
         autoclicker = {
-            amount = 0,
-            buns = 0.1,
-            cost = 10,
-            multiplier = 1.0
+            amount: 0,
+            buns: 0.1,
+            cost: 10,
+            multiplier: 1.0
         },
         children = {
-            amount = 0,
-            buns = 0.8,
-            cost = 50,
-            multiplier = 1.0
+            amount: 0,
+            buns: 0.8,
+            cost: 50,
+            multiplier: 1.0
         },
         dude = {
-            amount = 0,
-            buns = 6,
-            cost = 600,
-            multiplier = 1.0
+            amount: 0,
+            buns: 6,
+            cost: 600,
+            multiplier: 1.0
         },
         feet = {
-            amount = 0,
-            buns = 20,
-            cost = 4000,
-            multiplier = 1.0
+            amount: 0,
+            buns: 20,
+            cost: 4000,
+            multiplier: 1.0
         },
         cookie = {
-            amount = 0,
-            buns = 104,
-            cost = 44000,
-            multiplier = 1.0
+            amount: 0,
+            buns: 104,
+            cost: 44000,
+            multiplier: 1.0
         },
         shinobi = {
-            amount = 0,
-            buns = 563,
-            cost = 500000,
-            multiplier = 1.0
+            amount: 0,
+            buns: 563,
+            cost: 500000,
+            multiplier: 1.0
         },
         salveritie = {
-            amount = 0,
-            buns = 2925,
-            cost = 6500000,
-            multiplier = 1.0
+            amount: 0,
+            buns: 2925,
+            cost: 6500000,
+            multiplier: 1.0
         }
     },
-    fps = 30
+    fps: 30
 }
 
 function click() {
